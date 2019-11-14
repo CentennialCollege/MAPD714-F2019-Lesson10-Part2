683bc5ce8d0695267ba7dbc8268cc216931fd7cb
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2019-11-13 20:20:56 -0500

    Initial Commit

---
+++ MAPD714-F2019-Lesson10-Part2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MAPD714-F2019-Lesson10-Part2
+//
+//  Created by Tom Tsiliopoulos on 2019-11-13.
+//  Copyright © 2019 CentennialCollege. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
